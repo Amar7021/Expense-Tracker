6738e9fc265c18afeab8974b6905ca961b35345a
Amar7021 <amarhere1122@gmail.com>
2026-05-21 20:01:14 +0530

    wip

--- src/components/layouts/AuthRootLayout.jsx
+++ src/components/layouts/AuthRootLayout.jsx
@@ -1,11 +1,36 @@
-import { Navigate, Outlet } from "react-router"
+import { Navigate, Outlet, useLocation } from "react-router"
 import Header from "../common/header/Header"
 import Footer from "../common/footer/Footer"
 import { useAuth } from "@clerk/react"
-import BarLoading from "../loaders/BarLoading"
+import { useEffect, useRef } from "react"
+import LoadingBar from "react-top-loading-bar"
+import PuffLoading from "../loaders/PuffLoading"
 
 const AuthRootLayout = () => {
     const { isLoaded, userId } = useAuth()
+    const location = useLocation()
+
+    const loadingBarRef = useRef(null)
+
+    useEffect(() => {
+        const loadingBar = loadingBarRef.current
+
+        loadingBar?.continuousStart()
+
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+        })
+
+        const timer = setTimeout(() => {
+            loadingBar?.complete()
+        }, 500)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [location?.pathname])
 
     if (isLoaded && !userId) {
         return <Navigate to="/" replace />
@@ -13,8 +38,13 @@ const AuthRootLayout = () => {
 
     return (
         <>
+            <LoadingBar
+                color="var(--accent-1)"
+                ref={loadingBarRef}
+                shadow={true}
+            />
             <Header authRoute isLoading={!isLoaded} />
-            <main>{!isLoaded ? <BarLoading /> : <Outlet />}</main>
+            <main>{!isLoaded ? <PuffLoading /> : <Outlet />}</main>
             <Footer />
         </>
     )
--- src/components/layouts/RootLayout.jsx
+++ src/components/layouts/RootLayout.jsx
@@ -1,8 +1,19 @@
-import { Outlet } from "react-router"
+import { Outlet, useLocation } from "react-router"
 import Header from "../common/header/Header"
 import Footer from "../common/footer/Footer"
+import { useEffect } from "react"
 
 const RootLayout = () => {
+    const location = useLocation()
+
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+        })
+    }, [location?.pathname])
+
     return (
         <>
             <Header />
